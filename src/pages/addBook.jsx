2c65592039751a3bee5659b711976e738543f79d
whicencer <denielaz2020@gmail.com>
2021-11-16 17:09:14 +0200

    refactor: create page to add books

--- src/pages/addBook.jsx
+++ src/pages/addBook.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Input from '../components/UI/Input/Input';
+import Button from '../components/UI/Button/Button'
 
 const addBook = () => {
+    const [controlInput, setControlInput] = useState('')
     return (
-        <div>
-            Add Book
+        <div style={{maxWidth: '80%', margin: '20px auto'}}>
+            <Input type="text" placeholder="Enter the title of the book" value={controlInput} onChange={(e) => setControlInput(e.target.value)} />
+            <Button btnTitle="Search" />
         </div>
     )
 }
