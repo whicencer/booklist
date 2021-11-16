f6595fdda7576abc976b7f3b16c480b03e0005ef
whicencer <denielaz2020@gmail.com>
2021-11-16 23:00:19 +0200

    refactor: refactor addBook page

--- src/pages/addBook.jsx
+++ src/pages/addBook.jsx
@@ -1,13 +1,39 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import Input from '../components/UI/Input/Input';
 import Button from '../components/UI/Button/Button'
+import { getBooks } from '../store/reducers/Books';
+import { useSelector } from 'react-redux';
+import '../assets/styles/addBook.scss'
 
 const addBook = () => {
+    const dispatch = useDispatch()
+    const foundBooks = useSelector(state => state.books.foundBooks)
+
     const [controlInput, setControlInput] = useState('')
+
+    const clickHandler = () => {dispatch(getBooks(controlInput))}
     return (
-        <div style={{maxWidth: '80%', margin: '20px auto'}}>
-            <Input type="text" placeholder="Enter the title of the book" value={controlInput} onChange={(e) => setControlInput(e.target.value)} />
-            <Button btnTitle="Search" />
+        <div>
+            <div style={{maxWidth: '80%', margin: '20px auto'}}>
+                <Input type="text" placeholder="Enter the title of the book" value={controlInput} onChange={(e) => setControlInput(e.target.value)} />
+                <Button btnTitle="Search" handleClick={clickHandler} />
+            </div>
+            <div className="books">
+                {
+                    foundBooks.map((el, key) => {
+                        return (
+                            <div className="book" key={key}>
+                                <img src={el.volumeInfo.imageLinks
+                                    ? el.volumeInfo.imageLinks.smallThumbnail
+                                    : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNT0xwyLstvC7wH8jYIKur3GTcSq-g6fj2EbL4wk-qaONHYjBswa3rpFsZJeEjuXcG-lw&usqp=CAU'} alt="image" />
+                                <h2>{el.volumeInfo.title}</h2>
+                                <p>{el.volumeInfo.authors[0]}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
