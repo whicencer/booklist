f9821e20a5c84a7b3a8172fcdb58d4d37a2e0972
whicencer <denielaz2020@gmail.com>
2021-11-21 00:11:18 +0200

    refactor: add firebase request with thunk

--- src/store/reducers/Books.js
+++ src/store/reducers/Books.js
@@ -1,11 +1,28 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
+import { app } from '../../firebase';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getAuth } from '@firebase/auth';
+
 export const getBooks = createAsyncThunk(
     'Books/getBooks',
     async (query) => {
         const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`)
         const data = await response.json()
         return data
+    }
+)
+
+export const getBooksFromFire = createAsyncThunk(
+    'Books/getBooksFromFire',
+    async () => {
+        const db = getFirestore(app);
+        
+        const user = collection(db, `user_${getAuth().currentUser.uid}`);
+        const bookSnapshot = await getDocs(user);
+        const booksList = bookSnapshot.docs.map(doc => doc.data());
+
+        return booksList
     }
 )
 
@@ -24,7 +41,8 @@ const Books = createSlice({
         }
     },
     extraReducers: {
-        [getBooks.fulfilled]: (state, action) => {state.foundBooks = [...action.payload.items]}
+        [getBooks.fulfilled]: (state, action) => {state.foundBooks = [...action.payload.items]},
+        [getBooksFromFire.fulfilled]: (state, action) => {state.books = [...action.payload]}
     }
 })
 
