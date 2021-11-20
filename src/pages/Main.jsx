dfc2c6c5b7df83f235204f42386f1ca178998030
whicencer <denielaz2020@gmail.com>
2021-11-21 00:09:49 +0200

    refactor: delete firebase request from main component

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,25 +1,15 @@
 import React, {useEffect} from 'react';
 import BooksList from '../components/BooksList/BooksList';
 
-import { app } from '../firebase';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getAuth } from '@firebase/auth';
+import { getBooksFromFire } from '../store/reducers/Books';
 
 import { useDispatch } from 'react-redux';
-import { setBooks } from '../store/reducers/Books';
-import { useSelector } from 'react-redux';
 
 const Main = () => {
-    const db = getFirestore(app);
-    const books = useSelector(state => state.books.books)
     const dispatch = useDispatch()
 
-    useEffect(async () => {
-        const user = collection(db, `user_${getAuth().currentUser.uid}`);
-        const bookSnapshot = await getDocs(user);
-        const booksList = bookSnapshot.docs.map(doc => doc.data());
-        
-        dispatch(setBooks(booksList))
+    useEffect(() => {
+        dispatch(getBooksFromFire())
     }, [])
 
     return (
