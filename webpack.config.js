5af70ae9444027cc1dfaaf5acf63334d8dac8434
whicencer <denielaz2020@gmail.com>
2021-11-21 19:00:42 +0200

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
-        publicPath: 'booklist/'
+        publicPath: '/'
     },
     resolve: {
         extensions: ['.js', '.jsx']
