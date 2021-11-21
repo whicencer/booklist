15180da2db9cbb15d67375b1daf7b6d0b87a5fbb
whicencer <denielaz2020@gmail.com>
2021-11-21 17:30:16 +0200

    update routes

--- src/components/Navigation/NavigationAuth/NavigationAuth.jsx
+++ src/components/Navigation/NavigationAuth/NavigationAuth.jsx
@@ -4,8 +4,8 @@ import { NavLink } from 'react-router-dom';
 export const NavigationAuth = () => {
     return (
         <nav className="nav">
-            <NavLink className="nav-link" to='booklist/login'>Sign In</NavLink>
-            <NavLink className="nav-link" to='booklist/register'>Sign Up</NavLink>
+            <NavLink className="nav-link" to='/booklist/login'>Sign In</NavLink>
+            <NavLink className="nav-link" to='/booklist/register'>Sign Up</NavLink>
         </nav>
     )
 }
