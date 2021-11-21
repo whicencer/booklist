b1deb126ca3559e33fb0831cd9124e8684df82d1
whicencer <denielaz2020@gmail.com>
2021-11-21 18:44:42 +0200

    refactor

--- src/components/Navigation/NavigationAuth/NavigationAuth.jsx
+++ src/components/Navigation/NavigationAuth/NavigationAuth.jsx
@@ -4,13 +4,8 @@ import { NavLink } from 'react-router-dom';
 export const NavigationAuth = () => {
     return (
         <nav className="nav">
-<<<<<<< HEAD
             <NavLink className="nav-link" to='booklist/login'>Sign In</NavLink>
             <NavLink className="nav-link" to='booklist/register'>Sign Up</NavLink>
-=======
-            <NavLink className="nav-link" to='/booklist/login'>Sign In</NavLink>
-            <NavLink className="nav-link" to='/booklist/register'>Sign Up</NavLink>
->>>>>>> main
         </nav>
     )
 }
