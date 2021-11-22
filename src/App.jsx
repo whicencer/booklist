31e2159e11920f244c3a7a7961ac04dcff9cee5e
whicencer <denielaz2020@gmail.com>
2021-11-22 16:02:08 +0200

    update BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const App = () => {
           });
     }, [])
     return (
-        <BrowserRouter basename="/booklist">
+        <BrowserRouter basename="booklist">
             <Header />
             <Routers />
         </BrowserRouter>
